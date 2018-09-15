16cb76008e7ffcca1a35639dcd5cd291dd15f4cd
wanli <wanli@iot.chinamobile.com>
2018-09-15 14:47:16 +0800

    webpack dev server

--- build/webpack/webpack.dev.config.js
+++ build/webpack/webpack.dev.config.js
@@ -1,5 +1,10 @@
 var webpackBase = require('./webpack.base.config');
 var merge = require('webpack-merge')
+var path = require('path')
 module.exports = merge(webpackBase, {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000
+  }
 })
